a8fd5ff2cabe2e1338cac2551b8344418f98bd16
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-03-21 16:24:15 -0400

    var play between script and index

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,6 +6,7 @@ function doFunction() {
     //go to next page selected from array with if statements var check
     alert('look at that function function!');
     let urlEnd = "index.html";
+    countDownTrue = true; //trying to interact with in-line vars in index.html
 //does the if-ing
     //urlEnd = 
 //sends to     "https://cireneirbo.github.io/html-pages/" += urlEnd;
