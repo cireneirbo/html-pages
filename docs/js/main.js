94603f89d462aa25343bf4f8286c2f9aa464de72
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-03-21 16:17:27 -0400

    script called in index.html
    should use script when button pressed

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,4 +5,8 @@ const choiceArray = []; //array to push all site pages to
 function doFunction() {
     //go to next page selected from array with if statements var check
     alert('look at that function function!');
+    let urlEnd = "index.html";
+//does the if-ing
+    //urlEnd = 
+//sends to     "https://cireneirbo.github.io/html-pages/" += urlEnd;
 }
