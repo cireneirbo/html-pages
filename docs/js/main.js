e4bd52bf3fea2b7c9d028d01f3f8d7df875102b1
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-03-21 16:37:31 -0400

    trying another way
    var defined in index.html, accessing it in main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,7 +6,10 @@ function doFunction() {
     //go to next page selected from array with if statements var check
     alert('look at that function function!');
     let urlEnd = "index.html";
-    countDownTrue = true; //trying to interact with in-line vars in index.html
+    if (countDownTrue === false) {
+        console.log("This is the sort of thing that starts boss fights in video games, you know?...");
+    }
+    //countDownTrue = true; //trying to interact with in-line vars in index.html
 //does the if-ing
     //urlEnd = 
 //sends to     "https://cireneirbo.github.io/html-pages/" += urlEnd;
