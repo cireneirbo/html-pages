8d69c8bef4fac430c3b68a810a61ecbdfad15cdb
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-03-21 17:03:37 -0400

    dynamic text
    javascript writes to html

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,37 @@
 const choiceArray = []; //array to push all site pages to
-
+let page = 0;
 
 
 function doFunction() {
-    //go to next page selected from array with if statements var check
-    alert('look at that function function!');
-    let urlEnd = "index.html";
-    if (countDownTrue === false) {
+    //go to next content selected from array with if statements var check
+
+    //alert('look at that function function!');
+    //let urlEnd = "index.html";
+    /*if (countDownTrue === false) {
         console.log("This is the sort of thing that starts boss fights in video games, you know?...");
-    }
+    }*/
     //countDownTrue = true; //trying to interact with in-line vars in index.html
+
+
+
 //does the if-ing
     //urlEnd = 
 //sends to     "https://cireneirbo.github.io/html-pages/" += urlEnd;
+
+if (page === 1) { 
+    <img src="images/hey-at-least-i-made-it.jfif" alt="nothing to see here" class="beachImage"> <br>should have stayed home</img>
+    <p class="story-text">
+        All must heed the warning of the soothsayer, Promethius. 
+        He spoke of times when the denizens of the warm rock, Earth, would take to the high seas amid the chaos of the scurrying feet.
+    </p>
+}
+if (page === 2) {
+    <img src="images/the-saddest-mascot.jfif" alt="nothing to see here" class="beachImage"> <br>it was only a matter of time</img>
+    <p class="story-text">
+        ...Of when he ventured out to the beach from within the crass cityscape. 
+        The solar accompaniment was visibly intense, and the partakers of sand-based ruminations were donning wide brims and colored glasses.
+    </p>
+}
+
+
 }
